0f3404b9415a6c19ebeed21c8b3d8f74c9fc2042
Aleksander-developer <aleksandernikollibusiness@gmail.com>
2025-08-20 20:45:02 +0200

    cambio porta in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // src/index.ts
 import app from './app';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(` Server avviato su http://localhost:${PORT}`);
