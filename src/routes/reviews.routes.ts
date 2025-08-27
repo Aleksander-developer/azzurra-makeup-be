3752becc95b6c73874a583b4c8fb647e4636e5ec
Aleksander Nikolli <aleksandernikollibusiness@gmail.com>
2025-08-27 21:17:14 +0200

    Fix: Risolti errori di Place ID e configurazione API

--- src/routes/reviews.routes.ts
+++ src/routes/reviews.routes.ts
@@ -4,8 +4,12 @@ import { getReviews } from '../controllers/reviews.controller';
 
 const router = Router();
 
+// Gestisce esplicitamente la richiesta OPTIONS per il CORS
+router.options('/', (req, res) => {
+    res.status(200).send();
+});
+
 // Definisci la rotta GET per le recensioni
 router.get('/', getReviews);
 
 export default router;
-
