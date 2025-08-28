f64849a148da76cfd3837811a6b72a7ac3b26bc1
Aleksander Nikolli <aleksandernikollibusiness@gmail.com>
2025-08-28 11:06:44 +0200

    Fix: Risolto routing e configurazione API recensioni

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,15 +5,15 @@ import contattiRoutes from './contatti.routes';
 import serviziRoutes from './servizi.routes';
 import authRoutes from './auth.routes';
 import reviewsRoutes from './reviews.routes';
-// import portfolioRoutes from './portfolio.routes';
+import portfolioRoutes from './portfolio.routes';
 
 const router = express.Router();
 
 router.use('/chi-sono', chiSonoRoutes);
 router.use('/contatti', contattiRoutes);
 router.use('/servizi', serviziRoutes);
-// router.use('/portfolio', portfolioRoutes); 
-// router.use('/reviews', reviewsRoutes);
+router.use('/portfolio', portfolioRoutes); 
+router.use('/reviews', reviewsRoutes);
 
 
 router.use('/auth', authRoutes);
